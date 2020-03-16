51781822e73dd95b7cead135fd7ca0893e2345f4
Michael Chui <saraid216@gmail.com>
2020-03-15 23:25:29 -0700

    units: some imperials, and some time units

--- lib/units/of/length.rb
+++ lib/units/of/length.rb
@@ -5,6 +5,25 @@ module Units
 
   SI.expand_prefixes(Meter)
 
+  class Foot < Unit.of(:length)
+    def to_s; 'ft'; end
+    def conversions
+      { Yard => { method_names: %i( to_yards ),
+                  conversion: proc { |number| number.fdiv 3 } },
+        Mile => { method_names: %i( to_miles ),
+                  conversion: proc { |number| number.fdiv 5280 } }
+      }
+    end
+  end
+
+  class Yard < Unit.of(:length)
+    def to_s; ' yard'; end
+  end
+
+  class Mile < Unit.of(:length)
+    def to_s; ' mile'; end
+  end
+
   module OfLength
     extend Monkeypatchable
 
--- lib/units/of/time.rb
+++ lib/units/of/time.rb
@@ -17,6 +17,65 @@ module Units
     end
   end
 
+  class Hour < Unit.of(:time)
+    def to_s; ' hours'; end
+    def conversions
+      { Second => { method_names: %i( in_seconds ),
+                    conversion: proc { |n| n * 3600 } }
+      }
+    end
+  end
+
+  class Day < Unit.of(:time)
+    def to_s; ' days'; end
+    def conversions
+      { Second => { method_names: %i( in_seconds ),
+                    conversion: proc { |n| n * 3600 * 24 } }
+      }
+    end
+  end
+
+  class MeanTropicalYear < Unit.of(:time)
+    def to_s; ' mean tropical years'; end
+    def conversions
+      { Second => { method_names: %i( in_seconds ),
+                    conversion: proc { |n| n * 365.24219 * 86400 } }
+      }
+    end
+  end
+
+  class MeanGregorianYear < Unit.of(:time)
+    def to_s; ' mean Gregorian years'; end
+    def conversions
+      { Second => { method_names: %i( in_seconds ),
+                    conversion: proc { |n| n * 365.2425 * 86400 } }
+      }
+    end
+  end
+
+  class MeanJulianYear < Unit.of(:time)
+    def to_s; ' mean Julian years'; end
+    def conversions
+      { Second => { method_names: %i( in_seconds ),
+                    conversion: proc { |n| n * 365.25 * 86400 } }
+      }
+    end
+  end
+
+  class Annus < Unit.of(:time)
+    def to_s; 'a'; end
+  end
+  Annum = Annus
+
+  class Megaannum < Unit.of(:time)
+    def to_s; ' Ma'; end
+    def conversions
+      { Annum => { method_names: %i( in_annums ),
+                   conversion: proc { |n| n * 1_000_000 } }
+      }
+    end
+  end
+
   module OfTime
     extend Monkeypatchable
 
